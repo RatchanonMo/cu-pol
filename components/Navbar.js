df539e9e88c3220567ca2eea0796336c588845b2
RatchanonMo <49874@yupparaj.ac.th>
2023-07-11 22:11:32 +0700

    second changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,10 +4,12 @@ import { CgMenuRight } from "react-icons/cg";
 
 export default function Navbar() {
   return (
-    <div className="fixed z-50 flex w-full items-center justify-between bg-white py-2 px-4">
+    <div className="fixed z-50 flex w-full items-center justify-between bg-white px-4 py-2">
       <p className="text-xl font-bold">
         <GoDotFill className="inline text-6xl text-[#dd757c]" />
-        กลุ่มเครือข่ายผู้เปลี่ยนแปลงสังคม
+        <span className="hidden md:inline">
+          กลุ่มเครือข่ายผู้เปลี่ยนแปลงสังคม
+        </span>
       </p>
       <div>
         <CgMenuRight className="text-5xl" />
